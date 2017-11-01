dbeb9563d69d142701568daff4c784acc633201c
Clemente Martínez <clementemartinez.com@gmail.com>
2017-11-01 12:47:05 -0600

    Actualizando datos

--- views/template.php
+++ views/template.php
@@ -21,17 +21,17 @@
 			<div class="col-md-4">
 				<p class="text-center">01 (951) 205 1451</p>
 				<p class="text-center">contacto@daferconsultores.com</p>
+				<p class="text-center">Ave Hidalgo, 1706 int. 1, Centro, Oaxaca de Juárez, Oaxaca, Méx.</p>
 			</div>
 			<div class="col-md-4">
 				<p class="text-right">
-					<a href="#" target="_blank"><i class="fa fa-facebook fa-3x rs"></i></a>
-					<a href="#" target="_blank"><i class="fa fa-twitter fa-3x rs"></i></a>
-					<a href="#" target="_blank"><i class="fa fa-instagram fa-3x rs"></i></a>
+					<a href="https://www.facebook.com/Dafer-Consultores-616320678432628/" target="_blank"><i class="fa fa-facebook fa-3x rs"></i></a>
+					<a href="https://twitter.com/DaferFiscal" target="_blank"><i class="fa fa-twitter fa-3x rs"></i></a>
 				</p>
 			</div>
 
 		</div>
-		<p class="text-center">Diseñado por <a href="http://emtycorp.com.mx/software" target="_blank">Emty Software</a></p>
+		<p class="text-center"><i class="fa fa-code"></i> by <a href="http://emtycorp.com.mx/software" target="_blank">Emty Software</a></p>
 	</footer>
 	<script type="text/javascript" src="views/js/jquery.js"></script>
 	<script type="text/javascript" src="views/js/bootstrap.min.js"></script>
